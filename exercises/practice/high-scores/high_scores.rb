2b73b3bdc2ad54dd5e9ff9a1d9a2a6b0412b60f8
Timoti Fregni <timotif@gmail.com>
2026-08-07 18:21:08 +0200

    solve high-score exercise

--- exercises/practice/high-scores/high_scores.rb
+++ exercises/practice/high-scores/high_scores.rb
@@ -1,7 +1,23 @@
-=begin
-Write your code for the 'High Scores' exercise in this file. Make the tests in
-`high_scores_test.rb` pass.
+class HighScores
+  attr_reader :scores
+ 
+  def initialize(scores)
+    @scores = scores
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/high-scores` directory.
-=end
+  def latest
+    @scores[-1]
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.max(3)
+  end
+
+  def latest_is_personal_best?
+    latest == personal_best
+  end
+end
