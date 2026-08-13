2b77bd75363943ae8abb7c38778e7d90a2cf1078
Timoti Fregni <timotif@gmail.com>
2026-08-13 15:15:22 +0200

    Solve matrix exercise

--- exercises/practice/matrix/matrix.rb
+++ exercises/practice/matrix/matrix.rb
@@ -5,3 +5,17 @@ Write your code for the 'Matrix' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/matrix` directory.
 =end
+class Matrix
+  def initialize(matrix)
+    @data = matrix.split("\n").map { |row| row.split.map(&:to_i) }
+  end
+
+  def row(n)
+    @data[n - 1]
+  end
+
+  def column(n)
+    @data.transpose[n - 1]
+  end
+end
+
