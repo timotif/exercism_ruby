3219c1cb137caaafb187cb863259d8cf95ce09b4
Timoti Fregni <timotif@gmail.com>
2026-09-21 14:25:31 +0200

    Fix: Ensure proper return value (0) for zero/invalid input in EliudsEggs. egg_count

--- exercises/practice/eliuds-eggs/eliuds_eggs.rb
+++ exercises/practice/eliuds-eggs/eliuds_eggs.rb
@@ -5,3 +5,9 @@ Write your code for the 'Eliuds Eggs' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/eliuds-eggs` directory.
 =end
+class EliudsEggs
+  def self.egg_count(n)
+    n.to_s(2).count("1")
+  end
+end
+
